24b0f8032cf4c29e0713be7389dbdd4d51a94021
Bill Pliske <bpliske@gmail.com>
2021-05-27 21:27:45 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { hydrate, render } from 'react-dom';
 import { GlobalStyle } from './DefaultStyles';
 import App from './App';
 
-ReactDOM.render(
-    <>
-        <GlobalStyle />
-        <App />
-    </>,
-    document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+    hydrate(
+        <>
+            <GlobalStyle />
+            <App />
+        </>,
+        rootElement
+    );
+} else {
+    render(
+        <>
+            <GlobalStyle />
+            <App />
+        </>,
+        rootElement
+    );
+}
