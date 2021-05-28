9862d41061911c0767d5bd037a6694d050659fbe
Bill Pliske <bpliske@gmail.com>
2021-05-27 22:06:42 -0700

    updated

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import Menu from '../../components/Menu.jsx';
 
 export default function index() {
     return (
         <div>
             <h1>About page</h1>
+            <Image src="images/billcartoon_400x400.jpg" alt="cartoon" />
             <Menu />
         </div>
     );
 }
+
+const Image = styled.img`
+    width: 250px;
+`;
